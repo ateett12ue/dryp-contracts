a119cc0eca66c435efbcb876d9e8a6c2dcd09d1a
ateet <ateettiwari97@gmail.com>
2024-07-24 02:23:51 +0530

    treasyry task creaated

--- tasks/deploy/Treasury/Treasury.deploy.ts
+++ tasks/deploy/Treasury/Treasury.deploy.ts
@@ -1,51 +1,118 @@
-import hre from "hardhat";
+import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types";
+import {
+  CONTRACT_NAME,
+  DEFAULT_ENV,
+  DEPLOY_TREASURY_CONTRACT,
+  VERIFY_TREASURY_CONTRACT,
+  EXCHANGE_TOKEN,
+} from "../../constants";
+import { task } from "hardhat/config";
+import {
+  ContractType,
+  recordAllDeployments,
+  saveDeployments,
+  getDeployments,
+  IDeploymentAdapters,
+} from "../../utils";
 import ERC1967Proxy from "@openzeppelin/contracts/build/contracts/ERC1967Proxy.json";
-import ether from "ethers";
-import { EXCHANGE_TOKEN } from "../../constants";
 
-async function main() {
-  await hre.run("compile");
+const contractName: string = CONTRACT_NAME.Treasury;
+const contractType = ContractType.Intializable;
 
-  // deploy initial contract
-  const Treasury = await hre.ethers.getContractFactory("Treasury");
-  const myTreasury = await Treasury.deploy();
-  console.log("Treasury 1.0 deployed to:", myTreasury.address);
+task(DEPLOY_TREASURY_CONTRACT)
+  .addFlag("verify", "pass true to verify the contract")
+  .setAction(async function (
+    _taskArguments: TaskArguments,
+    _hre: HardhatRuntimeEnvironment
+  ) {
+    let env = process.env.ENV;
+    if (!env) env = DEFAULT_ENV;
 
-  // calculate initialize() call during deployment
-  const { abi } = await hre.artifacts.readArtifact("Treasury");
-  const iface = new ether.utils.Interface(abi);
-  const callInitialize = iface.encodeFunctionData("initialize", [
-    "__drypToken",
-    "__drypPool",
-    "0xBec33ce33afdAF5604CCDF2c4b575238C5FBD23d",
-    "0xBec33ce33afdAF5604CCDF2c4b575238C5FBD23d",
-    EXCHANGE_TOKEN[11155111].usdc.address,
-    EXCHANGE_TOKEN[11155111].usdt.address,
-  ]);
+    const network = await _hre.getChainId();
 
-  // deploy proxy
-  const Proxy = await hre.ethers.getContractFactory(
-    ERC1967Proxy.abi,
-    ERC1967Proxy.bytecode
-  );
-  const proxy = await Proxy.deploy(myTreasury.address, callInitialize);
-  console.log("Proxy deployed to:", proxy.address);
+    console.log(`Deploying ${contractName} Contract on chainId ${network}....`);
+    const factory = await _hre.ethers.getContractFactory("Treasury");
+    const treasury = await factory.deploy();
+    console.log("Treasury deployed to:", treasury.address);
+    await treasury.deployed();
+    const { abi } = await _hre.artifacts.readArtifact("Treasury");
+    const iface = new _hre.ethers.utils.Interface(abi);
 
-  // fs.writeFileSync(
-  //   "./status.json",
-  //   JSON.stringify(
-  //     { proxyAddress: proxy.address, myTreasury_v1Address: myTreasury.address },
-  //     "",
-  //     2
-  //   )
-  // );
-}
+    const deployments = getDeployments(contractType) as IDeploymentAdapters;
+    let drypToken;
+    for (let i = 0; i < deployments[env][network].length; i++) {
+      if (deployments[env][network][i].name === "Dryp") {
+        drypToken = deployments[env][network][i].address;
+        break;
+      }
+    }
+    const drypPool = drypToken;
+    const treasuryManager = "0xBec33ce33afdAF5604CCDF2c4b575238C5FBD23d";
 
-main()
-  .then(() => {
-    return 0;
-  })
-  .catch((error) => {
-    console.error("error", error);
-    // process.exit(1);
+    const callInitialize = iface.encodeFunctionData("initialize", [
+      drypToken,
+      drypPool,
+      treasuryManager,
+      EXCHANGE_TOKEN["11155111"].usdc.address,
+      EXCHANGE_TOKEN["11155111"].usdt.address,
+    ]);
+    console.log("Treasury Initialize", callInitialize);
+    const Proxy = await _hre.ethers.getContractFactory(
+      ERC1967Proxy.abi,
+      ERC1967Proxy.bytecode
+    );
+    const proxy = await Proxy.deploy(treasury.address, callInitialize);
+    console.log("Proxy deployed to:", proxy.address);
+
+    const deployment = await recordAllDeployments(
+      env,
+      network,
+      contractType,
+      contractName,
+      treasury.address
+    );
+
+    await saveDeployments(contractType, deployment);
+
+    const deploymentProxy = await recordAllDeployments(
+      env,
+      network,
+      ContractType.Proxy,
+      CONTRACT_NAME.TreasuryProxy,
+      proxy.address
+    );
+
+    await saveDeployments(contractType, deploymentProxy);
+
+    console.log(`${contractName} contract deployed at`, treasury.address);
+    console.log(`_taskArguments`, _taskArguments);
+    if (_taskArguments.verify === true) {
+      await _hre.run(VERIFY_TREASURY_CONTRACT);
+    }
   });
+
+task(VERIFY_TREASURY_CONTRACT).setAction(async function (
+  _taskArguments: TaskArguments,
+  _hre: HardhatRuntimeEnvironment
+) {
+  let env = process.env.ENV;
+  if (!env) env = DEFAULT_ENV;
+
+  const network = await _hre.getChainId();
+
+  const deployments = getDeployments(contractType) as IDeploymentAdapters;
+  let address;
+  for (let i = 0; i < deployments[env][network].length; i++) {
+    if (deployments[env][network][i].name === contractName) {
+      address = deployments[env][network][i].address;
+      break;
+    }
+  }
+  console.log(`Verifying ${contractName} Contract....`);
+  await _hre.run("verify:verify", {
+    address,
+    constructorArguments: ["DRYP", "dryp"],
+  });
+
+  console.log(`Verified ${contractName} contract address `, address);
+});
